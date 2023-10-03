10f7139919efa72606e4705faadef242b2569229
Ramah Hashem Madi <ramahmadi2001@gmail.com>
2023-10-04 01:00:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, string){
+     const driverName = drivers.filter(name => string.toLowerCase() === name.toLowerCase())
+     return driverName
+}
+
+function fuzzyMatch(drivers, string){
+     const fuzzyName = drivers.filter(name => name.toLowerCase().startsWith(string.toLowerCase()))
+     return fuzzyName
+}
+
+function matchName(drivers, string){
+    const matchDriver = drivers.filter(nameToCompare => nameToCompare["name"].toLowerCase() === string.toLowerCase())
+    return matchDriver
+}
